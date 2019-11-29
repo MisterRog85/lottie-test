784af21d9c832880a4d0094eebdebb2ea1378f52
William Tomas <william.tomas@mobiapps.fr>
2019-11-29 09:49:08 +0100

    Initial Commit

---
+++ test-lottie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-lottie
+//
+//  Created by William Tomas on 29/11/2019.
+//  Copyright © 2019 William Tomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
