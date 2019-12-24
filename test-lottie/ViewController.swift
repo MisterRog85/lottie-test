747ff94036048cbcc73b07541d96948d16d01915
William Tomas <william.tomas@mobiapps.fr>
2019-12-24 15:32:46 +0100

    premier commit

--- test-lottie/ViewController.swift
+++ test-lottie/ViewController.swift
@@ -17,58 +17,47 @@ class ViewController: UIViewController {
         
     //création des animationview, on passe en param le nom de l'animation (sans l'extension)
     let animationView = AnimationView(name: "outline")
-    let animationViewReverse = AnimationView(name: "outline-reverse")
             
     var played: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        staticAnim(played)
+        staticAnim()
     }
 
     //coeur fixe
-    func staticAnim(_ type: Bool) {
-        if !type {
-            animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-            animationView.center = self.view.center
-            animationView.backgroundColor = .purple
-            animationView.contentMode = .scaleAspectFill
-            //animationView.makeCircular()
-            self.view.addSubview(animationView)
+    func staticAnim() {
+        
+        animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        animationView.center = self.view.center
+        animationView.backgroundColor = .purple
+        animationView.contentMode = .scaleAspectFill
+        //animationView.makeCircular()
+        self.view.addSubview(animationView)
 
-            let gesture = UITapGestureRecognizer(target: self, action: #selector (actionAnim(_:)))
+        let gesture = UITapGestureRecognizer(target: self, action: #selector (actionAnim(_:)))
 
-            animationView.addGestureRecognizer(gesture)
-        } else {
-            animationViewReverse.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-            animationViewReverse.center = self.view.center
-            animationViewReverse.backgroundColor = .purple
-            animationViewReverse.contentMode = .scaleAspectFill
-            //animationView.makeCircular()
-            self.view.addSubview(animationViewReverse)
-
-            let gesture = UITapGestureRecognizer(target: self, action: #selector (actionAnim(_:)))
-
-            animationViewReverse.addGestureRecognizer(gesture)
-        }
+        animationView.addGestureRecognizer(gesture)
 
     }
 
     //animation pour le dislike
     func reverseAnim() {
-        staticAnim(played)
-        animationViewReverse.play()
+        staticAnim()
+        //animationViewReverse.play()
+        animationView.play(fromProgress: 1, toProgress: 0)
+
     }
 
     //animation
     func playAnim() {
-        staticAnim(played)
-        animationView.play()
+        staticAnim()
+        animationView.play(fromProgress: 0, toProgress: 1)
     }
     
     @objc func actionAnim(_ gestureRecognizer:UITapGestureRecognizer) {
-        if played {
+          if played {
               reverseAnim()
               played = false
           } else {
